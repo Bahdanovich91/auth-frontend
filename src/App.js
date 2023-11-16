8fd105e0bffc2b4883305f58408539dbdecd2e02
Bahdanovich <bahdanovich@internet.ru>
2023-11-17 00:16:45 +0300

    Added styles for the form

--- src/App.js
+++ src/App.js
@@ -1,58 +1,59 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isRegisterMode, setIsRegisterMode] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isRegisterMode, setIsRegisterMode] = useState(false);
 
-  const handleAuth = async () => {
-    try {
-      const endpoint = isRegisterMode ? 'register' : 'login';
-      const response = await fetch(`http://localhost/${endpoint}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      });
+    const handleAuth = async () => {
+        try {
+            const endpoint = isRegisterMode ? 'register' : 'login';
+            const response = await fetch(`http://localhost/${endpoint}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-      <div>
-        <h2>{isRegisterMode ? 'Регистрация' : 'Вход'}</h2>
-        <label>Email:</label>
-        <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Password:</label>
-        <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleAuth}>{isRegisterMode ? 'Зарегистрироваться' : 'Войти'}</button>
-        <p onClick={() => setIsRegisterMode(!isRegisterMode)}>
-          {isRegisterMode ? 'Уже есть аккаунт? Войти.' : 'Нет аккаунта? Зарегистрироваться.'}
-        </p>
-      </div>
-  );
+    return (
+        <div className="auth-container">
+            <h2>{isRegisterMode ? 'Регистрация' : 'Вход'}</h2>
+            <label>Email:</label>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password:</label>
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleAuth}>{isRegisterMode ? 'Зарегистрироваться' : 'Войти'}</button>
+            <p onClick={() => setIsRegisterMode(!isRegisterMode)}>
+                {isRegisterMode ? 'Уже есть аккаунт? Войти.' : 'Нет аккаунта? Зарегистрироваться.'}
+            </p>
+        </div>
+    );
 };
 
 export default App;
