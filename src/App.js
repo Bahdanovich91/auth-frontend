a26a7726faa5f3cc1ccaa4a62f3ab7cd6c10be0e
Bahdanovich <bahdanovich@internet.ru>
2023-11-19 10:38:23 +0300

    Added fields for register. Style fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
     const [isRegisterMode, setIsRegisterMode] = useState(false);
 
     const handleAuth = async () => {
         try {
             const endpoint = isRegisterMode ? 'register' : 'login';
-            const response = await fetch(`http://localhost/${endpoint}`, {
+            const requestData = isRegisterMode
+                ? { name, email, password, password_confirmation: passwordConfirmation }
+                : { email, password };
+
+            const response = await fetch(`http://localhost/api/${endpoint}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({
-                    email: email,
-                    password: password,
-                }),
+                body: JSON.stringify(requestData),
             });
 
             if (!response.ok) {
@@ -34,6 +37,19 @@ function App() {
     return (
         <div className="auth-container">
             <h2>{isRegisterMode ? 'Регистрация' : 'Вход'}</h2>
+
+            {isRegisterMode && (
+                <>
+                    <label>Name:</label>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </>
+            )}
+
             <label>Email:</label>
             <input
                 type="email"
@@ -41,6 +57,7 @@ function App() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
             />
+
             <label>Password:</label>
             <input
                 type="password"
@@ -48,9 +65,26 @@ function App() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
-            <button onClick={handleAuth}>{isRegisterMode ? 'Зарегистрироваться' : 'Войти'}</button>
+
+            {isRegisterMode && (
+                <>
+                    <label>Password Confirmation:</label>
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        value={passwordConfirmation}
+                        onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    />
+                </>
+            )}
+
+            <button onClick={handleAuth}>
+                {isRegisterMode ? 'Зарегистрироваться' : 'Войти'}
+            </button>
             <p onClick={() => setIsRegisterMode(!isRegisterMode)}>
-                {isRegisterMode ? 'Уже есть аккаунт? Войти.' : 'Нет аккаунта? Зарегистрироваться.'}
+                {isRegisterMode
+                    ? 'Уже есть аккаунт? Войти.'
+                    : 'Нет аккаунта? Зарегистрироваться.'}
             </p>
         </div>
     );
